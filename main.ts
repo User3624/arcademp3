ca82f6da46582f055c6b804f749ee165631fb3e2
User3824 <108425476+User3824@users.noreply.github.com>
2023-06-14 18:36:47 +0300

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,59 @@
 namespace SpriteKind {
     export const Desktop = SpriteKind.create()
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (cursor.overlapsWith(MusicICO)) {
+        cursor.sayText("In development.", 500, false)
+    }
+    if (cursor.overlapsWith(SettingsICO)) {
+        cursor.sayText("In development.", 500, false)
+    }
+    if (cursor.overlapsWith(VideoICO)) {
+        cursor.sayText("In development.", 500, false)
+    }
+    if (cursor.overlapsWith(Power)) {
+        RestartOption = sprites.create(assets.image`ResetOption`, SpriteKind.Desktop)
+        sprites.destroy(cursor)
+        cursor = sprites.create(assets.image`crosshair`, SpriteKind.Player)
+        controller.moveSprite(cursor)
+        RestartOption.setPosition(63, 18)
+    }
+    if (cursor.overlapsWith(RestartOption)) {
+        music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.UntilDone)
+        game.reset()
+    }
+})
+let RestartOption: Sprite = null
+let cursor: Sprite = null
+let Power: Sprite = null
+let VideoICO: Sprite = null
+let SettingsICO: Sprite = null
+let MusicICO: Sprite = null
 scene.setBackgroundImage(assets.image`bg`)
 music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.UntilDone)
 game.splash("Warning: This ROM is in its early development stages and might be unstable.")
-let MusicICO = sprites.create(assets.image`musicico`, SpriteKind.Desktop)
+MusicICO = sprites.create(assets.image`musicico`, SpriteKind.Desktop)
 MusicICO.setPosition(9, 9)
-let SettingsICO = sprites.create(assets.image`SettingsICO`, SpriteKind.Desktop)
+SettingsICO = sprites.create(assets.image`SettingsICO`, SpriteKind.Desktop)
 SettingsICO.setPosition(27, 9)
-let VideoICO = sprites.create(assets.image`VideoICO`, SpriteKind.Desktop)
+VideoICO = sprites.create(assets.image`VideoICO`, SpriteKind.Desktop)
 VideoICO.setPosition(45, 9)
-let cursor = sprites.create(assets.image`cursor-i-guess`, SpriteKind.Player)
+Power = sprites.create(assets.image`power`, SpriteKind.Desktop)
+Power.setPosition(63, 9)
+cursor = sprites.create(assets.image`crosshair`, SpriteKind.Player)
 controller.moveSprite(cursor)
+cursor.setStayInScreen(true)
+game.onUpdateInterval(500, function () {
+    if (cursor.overlapsWith(MusicICO)) {
+        cursor.sayText("Music", 500, false)
+    }
+    if (cursor.overlapsWith(SettingsICO)) {
+        cursor.sayText("Settings", 500, false)
+    }
+    if (cursor.overlapsWith(VideoICO)) {
+        cursor.sayText("Video", 500, false)
+    }
+    if (cursor.overlapsWith(Power)) {
+        cursor.sayText("Power Options", 500, false)
+    }
+})
